f57329a1fc24fa813bf612964b8102e373dee3d5
craigsanders <crasanders@gmail.com>
2019-03-15 15:48:44 -0500

    rewrote modeling routine

--- model_vet.py
+++ model_vet.py
@@ -1,67 +1,100 @@
+import pickle
 import numpy as np
 import pandas as pd
-from scipy.spatial.distance import pdist, squareform
+from scipy.spatial.distance import cdist
+from hyperopt import fmin, tpe, hp, STATUS_OK, STATUS_FAIL, Trials
 
-vet_trials = pd.read_csv('vet_trials.txt', delim_whitespace=True)
-images = np.loadtxt('images.txt', dtype=str)
+vet_trials = pd.read_csv('vet_trials.csv').query('Task == "Different"')
 
-model_responses = []
+categories = vet_trials['Category'].unique()
+layers = ['block1_pool', 'block2_pool', 'block3_pool', 'block4_pool', 'block5_pool', 'fc1', 'fc2', 'predictions']
+metrics = ['euclidean', 'cityblock', 'cosine', 'jaccard']
 
-for net in ['vgg16', 'inceptionv3', 'resnet50', 'densenet201']:
-    for cleaned in [True, False]:
-        rep = net
-        if cleaned:
-            rep = 'cleaned_' + net
+with open('features.pkl', 'rb') as file:
+    features = pickle.load(file)
 
-        else:
-            rep = 'raw_' + net
+def model_predictions(layer, metric, c, gamma, biases, memory):
+    predictions = []
+    for i, row in data.iterrows():
+        trial_exemplars = []
+        for j in range(1, 7):
+            ex = row['exemplar{}'.format(j)]
+            ex_features = features[ex][layer]
+            trial_exemplars.append(ex_features)
+        trial_exemplars = np.array(trial_exemplars)
 
-        features = np.loadtxt('vet_{}.txt'.format(rep))
+        target = row['tarname']
+        dist1 = row['dist1name']
+        dist2 = row['dist2name']
 
-        distances = pdist(features)
-        distances = squareform(distances)
-        distances /= np.max(distances)
+        target_features = features[target][layer]
+        dist1_features = features[dist1][layer]
+        dist2_features = features[dist2][layer]
 
-        indices = {}
-        for i, im in enumerate(images):
-            indices[im] = i
+        # trial_choices = [dist1_features, dist2_features]
+        # trial_choices.insert(row['tarloc'] - 1, target_features)
+        trial_choices = [target_features, dist1_features, dist2_features]
+        trial_choices = np.array(trial_choices)
 
-        n_exemplars = 6
-        n_choices = 3
+        distances = cdist(trial_exemplars, trial_choices, metric=metric)
+        sims = np.exp(-c * distances)
+        weightedsims = (sims.T * memory).T
+        sumsims = weightedsims.sum(axis=0)
+        weightedsums = biases * sumsims ** gamma
+        probs = weightedsums / weightedsums.sum()
 
-        for log_c in np.arange(0, 3, .1):
-            c = 10 ** log_c
-            probs = np.zeros((vet_trials.shape[0], n_choices))
+        predictions.append(probs)
+    return np.array(predictions)
 
-            for t, row in vet_trials.iterrows():
-                target = indices[row['tarname']]
-                distractor1 = indices[row['dist1name']]
-                distractor2 = indices[row['dist2name']]
-                choices = [target, distractor1, distractor2]
+def model_loss(space):
+    # biases = np.array([space['b1'], space['b2'], 1])
+    biases = np.array([1.] * 3)
+    biases /= biases.sum()
 
-                trial_distances = np.zeros((n_exemplars, n_choices))
+    memory = np.array([space['v1'], space['v2'], space['v3'], space['v4'], space['v5'], 1])
+    # memory = np.array([1.] * 6)
+    memory /= memory.sum()
 
-                for i in range(n_exemplars):
-                    exemplar = indices[row['exemplar{}'.format(i+1)]]
-                    for j in range(n_choices):
-                        trial_distances[i, j] = distances[exemplar, choices[j]]
+    predictions = model_predictions(space['layer'], space['metric'], space['c'], space['gamma'], biases, memory)
 
-                similarities = np.exp(-c * trial_distances)
-                summed_similarities = np.sum(similarities, axis=0)
-                trial_probs = summed_similarities / np.sum(summed_similarities)
-                # trial_probs[0] = trial_probs[0] == np.max(trial_probs)
+    responses = np.array(data.loc[:, 'resp1':'resp3'])
+    nll = -np.sum(np.log(predictions) * responses)
 
-                probs[t] = trial_probs.reshape(n_choices)
+    if not np.isnan(nll):
+        status = STATUS_OK
+    else:
+        status = STATUS_FAIL
 
-            probs = pd.DataFrame(probs, columns=['target_response', 'dist1response', 'dist2response'])
-            probs['Representation'] = net
-            probs['Cleaned'] = cleaned
+    mean_error = 1 - predictions[:, 0].mean()
 
-            model_response = pd.concat((vet_trials, probs), axis=1)
-            model_response['log_c'] = log_c
-            model_response['c'] = c
-            model_responses.append(model_response)
+    return {'loss': mean_error, 'status': status, 'predictions': predictions}
 
-all_models = pd.concat(model_responses)
-all_models.to_csv('model_results.csv')
 
+space = {
+         'c': hp.loguniform('c', -2, 2),
+         'gamma': hp.loguniform('gamma', -2, 2),
+         'layer': hp.choice('layer', layers),
+         'metric': hp.choice('metric', metrics)
+        }
+
+# for i in range(1, 3):
+#     space['b{}'.format(i)] = hp.uniform('b{}'.format(i), 0, 10)
+
+for i in range(1, 6):
+    space['v{}'.format(i)] = hp.uniform('v{}'.format(i), 0, 10)
+
+best_results = {}
+for category in categories:
+    data = vet_trials[vet_trials['Category'] == category]
+    trials = Trials()
+    best_parms = fmin(model_loss, space=space, algo=tpe.suggest, max_evals=10000, trials=trials)
+    best_index = np.nanargmin(trials.losses())
+    best_predictions = trials.results[best_index]['predictions']
+    best_loss = trials.results[best_index]['loss']
+
+    best_results[category] = {}
+    best_results[category]['params'] = best_parms
+    best_results[category]['predictions'] = best_predictions
+    best_results[category]['loss'] = best_loss
+
+    print(category, 1-best_loss)
