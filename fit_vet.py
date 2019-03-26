741c4ef24792783d4a10e79ba1884822878001b4
craigsanders <crasanders@gmail.com>
2019-03-26 17:34:40 -0500

    correctly fits mds representations

--- fit_vet.py
+++ fit_vet.py
@@ -6,65 +6,78 @@ from scipy.special import logsumexp
 from scipy.optimize import minimize
 
 vet_trials = pd.read_csv('vet_trials.csv')
+categories = np.loadtxt('categories.txt', dtype=str)
+cats = sorted(np.unique(categories))
+images = np.loadtxt('images.txt', dtype=str)
 
-for category in sorted(vet_trials['Category'].unique()):
-    data = vet_trials.query('Task == "Different" and Category == "{}"'.format(category))
-    mds = np.loadtxt('vet_{}_vgg16_mds.txt'.format(category), delimiter=',')
-    mds /= mds.max()
+vet_trials = pd.read_csv('vet_trials.csv')
+data = vet_trials.query('Task == "Different"')
 
-    directory = 'VET Cleaned'
-    features = {}
-    i = 0
-    for subdir, dirs, files in os.walk(directory):
-        dirs.sort()
-        for file in sorted(files):
-            if file.endswith(".bmp") and subdir.split('/')[1] == category:
-                    features[file] = mds[i]
-                    i += 1
+mds = []
+for category in cats:
+    cat_mds = np.loadtxt('vet_{}_vgg16_mds_20.txt'.format(category), delimiter=',')
+    mds.append(cat_mds)
+mds = np.concatenate(mds)
+
+dimminusone = mds.shape[1] - 1
+
+indices = {}
+for i, im in enumerate(images):
+    indices[im] = i
 
 
-    def model(parms):
-        w = np.append(parms, 1)
+def model(parms):
+    offset = 0
+    features = []
+    for category in cats:
+        w = np.append(parms[offset:offset + dimminusone], 1)
+        features.append(mds[categories == category] * w)
+        offset += dimminusone
+    features = np.concatenate(features)
 
-        nll = 0
-        acc = []
-        for i, row in data.iterrows():
-            trial_exemplars = []
-            for j in range(1, 7):
-                ex = row['exemplar{}'.format(j)]
-                ex_features = features[ex] * w
-                trial_exemplars.append(ex_features)
-            trial_exemplars = np.array(trial_exemplars)
+    nll = 0
+    acc = []
+    probabilities = []
+    for i, row in data.iterrows():
+        trial_exemplars = []
+        for j in range(1, 7):
+            ex = indices[row['exemplar{}'.format(j)]]
+            ex_features = features[ex]
+            trial_exemplars.append(ex_features)
+        trial_exemplars = np.array(trial_exemplars)
 
-            target = row['tarname']
-            dist1 = row['dist1name']
-            dist2 = row['dist2name']
+        target = indices[row['tarname']]
+        dist1 = indices[row['dist1name']]
+        dist2 = indices[row['dist2name']]
 
-            tarloc = row['tarloc'] - 1
+        tarloc = row['tarloc'] - 1
 
-            target_features = features[target] * w
-            dist1_features = features[dist1] * w
-            dist2_features = features[dist2] * w
+        target_features = features[target]
+        dist1_features = features[dist1]
+        dist2_features = features[dist2]
 
-            trial_choices = [dist1_features, dist2_features]
-            trial_choices.insert(tarloc, target_features)
+        trial_choices = [dist1_features, dist2_features]
+        trial_choices.insert(tarloc, target_features)
 
-            distances = cdist(trial_exemplars, trial_choices)
-            sims = np.exp(-distances)
-            sumsims = sims.sum(axis=0)
-            probs = sumsims / sumsims.sum()
+        distances = cdist(trial_exemplars, trial_choices)
+        probs = np.exp(logsumexp(distances, axis=0) - logsumexp(distances))
+        probabilities.append(probs)
 
-            # targets = [0, 0]
-            # targets.insert(tarloc, 1)
-            targets = np.array(row['resp1':'resp3'])
-            nll -= np.sum(np.log(probs) * np.array(targets))
+        targets = np.array(row['resp1':'resp3'])
+        # targets = [0, 0]
+        # targets.insert(tarloc, 1)
+        nll -= np.sum(np.log(probs) * np.array(targets))
 
-            acc.append(probs[row['highest_resp']-1])
+        acc.append(probs[row['highest_resp']-1])
+        # acc.append(probs[tarloc])
 
-            return nll, np.array(acc).mean()
+    probabilities = np.array(probabilities)
+    acc = np.array(acc)
+    return nll, probabilities, acc, acc.mean()
 
-    fit = minimize(lambda x: model(x)[0], [1]*(7), bounds=[(0, None)]*(7))
-    print(category, model(fit.x))
+
+fit = minimize(lambda x: model(x)[0], [1]*dimminusone*len(cats), bounds=[(0, None)]*dimminusone*len(cats))
+print(model(fit.x)[-1])
 
 # import seaborn as sns
 # import matplotlib.pyplot as plt
