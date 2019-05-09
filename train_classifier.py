dc18816a43f8aac29578905de8d53f4e38b144fc
craigsanders <crasanders@gmail.com>
2019-05-09 13:01:51 -0500

    updated classifier training code

--- train_classifier.py
+++ train_classifier.py
@@ -1,91 +1,39 @@
-from keras.layers import Dense, Dropout
-from keras.layers.normalization import BatchNormalization
-from keras.models import Model
-from keras.optimizers import Adam
+from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator
-from keras.applications.densenet import DenseNet121, preprocess_input
+from keras.applications.densenet import preprocess_input
 from keras.callbacks import ModelCheckpoint
-from keras.utils import multi_gpu_model
-from hyperopt import Trials, STATUS_OK, tpe
-from hyperas import optim
-from hyperas.distributions import choice, uniform
-from os.path import join
 import numpy as np
+from os.path import join
 
+save_dir = 'Plane Classifiers'
+npix = 224
+imdir = 'Processed Planes'
+batch = 64
 
-def data():
-    npix = 224
-    imdir = 'Processed Planes'
-    batch = 64
+datagen = ImageDataGenerator(
+    preprocessing_function=preprocess_input,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    validation_split=.1)
 
-    datagen = ImageDataGenerator(
-        preprocessing_function=preprocess_input,
-        rotation_range=20,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True,
-        validation_split=.1)
+train_gen = datagen.flow_from_directory(imdir, class_mode='categorical', target_size=(npix, npix),
+                                        subset='training', batch_size=batch)
+val_gen = datagen.flow_from_directory(imdir, class_mode='categorical', target_size=(npix, npix),
+                                      subset='validation', batch_size=batch)
 
-    train_gen = datagen.flow_from_directory(imdir, class_mode='categorical', target_size=(npix, npix),
-                                            subset='training', batch_size=batch)
-    val_gen = datagen.flow_from_directory(imdir, class_mode='categorical', target_size=(npix, npix),
-                                          subset='validation', batch_size=batch)
+hyperparms = [2, 256, -2.0286623630413607]
+model = load_model(join(save_dir, '{}.hdf5'.format(hyperparms)))
 
-    return train_gen, val_gen
+checkpoint = ModelCheckpoint('plane_classifier_classifier.hdf5'.format(hyperparms), save_best_only=True)
 
+result = model.fit_generator(train_gen,
+                             epochs=100,
+                             validation_data=val_gen,
+                             callbacks=[checkpoint],
+                             verbose=True)
 
-def create_model(train_gen, val_gen):
-    save_dir = 'Plane Classifiers'
-    ncat = 25
-    dropout = .5
-
-    nlayers = {{choice([0, 1, 2, 3, 4])}}
-    nunits = {{choice([64, 128, 256, 512, 1024])}}
-    loglr = {{uniform(-5, -1)}}
-    lr = 10 ** loglr
-
-    hyperparms = [nlayers, nunits, loglr]
-
-    base_model = DenseNet121(include_top=False, pooling='avg')
-
-    for layer in base_model.layers:
-        layer.trainable = False
-
-    x = base_model.output
-
-    for lyr in range(nlayers):
-        x = Dense(nunits, activation='relu')(x)
-        x = BatchNormalization()(x)
-        x = Dropout(dropout)(x)
-
-    x = Dense(ncat, activation='softmax')(x)
-
-    model = Model(inputs=base_model.input, outputs=x)
-    model = multi_gpu_model(model, gpus=2)
-
-    model.compile(optimizer=Adam(lr), loss='categorical_crossentropy', metrics=['acc'])
-
-    checkpoint = ModelCheckpoint(join(save_dir, '{}.hdf5'.format(hyperparms)), save_best_only=True)
-
-    result = model.fit_generator(train_gen,
-                                 epochs=60,
-                                 validation_data=val_gen,
-                                 callbacks=[checkpoint],
-                                 verbose=False)
-
-    validation_acc = np.amax(result.history['val_acc'])
-    with open('plane_classifier_results.txt', 'a') as f:
-        f.write(str(validation_acc) + ' ' + str(hyperparms) + '\n')
-    return {'loss': -validation_acc, 'status': STATUS_OK, 'model': model}
-
-
-if __name__ == '__main__':
-    best_run, best_model = optim.minimize(model=create_model,
-                                                data=data,
-                                                algo=tpe.suggest,
-                                                max_evals=25,
-                                                trials=Trials())
-
-    save_dir = 'Plane Classifiers'
-    best_model.save(join(save_dir, 'best_model.hdf5'))
+np.savetxt('pc_classifier_training_acc.txt', result.history['acc'])
+np.savetxt('pc_classifier_validation_acc.txt', result.history['val_acc'])
--- train_classifier_accre.py
+++ train_classifier_accre.py
@@ -1,28 +1,15 @@
-from keras.layers import Dense
-from keras.models import Model
+from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator
-from keras.applications.resnet50 import ResNet50, preprocess_input
-from keras.utils import multi_gpu_model
+from keras.applications.densenet import preprocess_input
 from keras.callbacks import ModelCheckpoint
-from math import ceil
 import numpy as np
+from math import ceil
 
-nepoch = 200
+save_dir = 'Plane Classifiers'
 npix = 224
-batch_size = 64
 imdir = 'Planes'
-
-base_model = ResNet50(include_top=False, pooling='avg')
-
-for layer in base_model.layers:
-    layer.trainable = False
-
-x = base_model.output
-x = Dense(25, activation='softmax')(x)
-
-model = Model(inputs=base_model.input, outputs=x)
-model = multi_gpu_model(model, gpus=2)
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
+batch = 64
+nepoch = 100
 
 datagen = ImageDataGenerator(
     preprocessing_function=preprocess_input,
@@ -33,28 +20,23 @@ datagen = ImageDataGenerator(
     horizontal_flip=True,
     validation_split=.1)
 
-train_gen = datagen.flow_from_directory(imdir, class_mode='categorical', target_size=(npix, npix), subset='training',
-                                        batch_size=batch_size)
-val_gen = datagen.flow_from_directory(imdir, class_mode='categorical', target_size=(npix, npix), subset='validation',
-                                      batch_size=batch_size)
+train_gen = datagen.flow_from_directory(imdir, class_mode='categorical', target_size=(npix, npix),
+                                        subset='training', batch_size=batch)
+val_gen = datagen.flow_from_directory(imdir, class_mode='categorical', target_size=(npix, npix),
+                                      subset='validation', batch_size=batch)
 
-checkpoint = ModelCheckpoint('plane_classifier.hdf5', save_best_only=True)
+hyperparms = [2, 256, -2.0286623630413607]
+model = load_model('{}.hdf5'.format(hyperparms))
+
+checkpoint = ModelCheckpoint('plane_classifier_classifier.hdf5'.format(hyperparms), save_best_only=True)
 
 history = model.fit_generator(train_gen,
                               epochs=nepoch,
                               validation_data=val_gen,
-                              steps_per_epoch=ceil(7353 / batch_size),
-                              validation_steps=ceil(805 / batch_size),
+                              steps_per_epoch=ceil(7353 / batch),
+                              validation_steps=ceil(805 / batch),
                               callbacks=[checkpoint]
                               )
 
-np.savetxt('plane_classifier_training_acc.txt', history.history['acc'])
-np.savetxt('plane_classifier_validation_acc.txt', history.history['val_acc'])
-
-#df = pd.DataFrame({'Epoch':list(range(1, nepoch+1)), 'Training': history.history['acc'], 'Validation': history.history['val_acc']})
-#df = pd.melt(df, 'Epoch', ['Training', 'Validation'], var_name='Set', value_name='Accuracy')
-#p = sns.lineplot(x='Epoch', y='Accuracy', hue='Set', data=df)
-#plt.ylim(0, 1)
-#plt.savefig('plane_classifier_accuracy.pdf')
-
-
+np.savetxt('pc_classifier_training_acc.txt', history.history['acc'])
+np.savetxt('pc_classifier_validation_acc.txt', history.history['val_acc'])
